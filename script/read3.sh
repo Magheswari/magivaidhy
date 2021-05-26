a0139e75d6f44999d5e9045b837c28a13881a082
magivaidhy <magivaidhy87@gmail.com>
2021-05-26 20:10:32 +0530

    updated scripts

---
+++ script/read3.sh
@@ -0,0 +1,4 @@
+echo 'Enter DevOps Tools: '
+read -a devopstools
+echo "The DevOps Tools which you entered are: " ${devopstools[0]},${devopstools[1]},${devopstools[2]},${devopstools[3]},${devopstools[4]}
+
